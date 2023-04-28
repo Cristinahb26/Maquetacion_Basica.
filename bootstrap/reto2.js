3f5ed976473a3caf5b4f005e1476798a8cede703
Cristinahb <achbocourt@gmail.com>
2023-04-28 11:36:31 +0200

    guardar cambios

--- bootstrap/reto2.js
+++ bootstrap/reto2.js
@@ -1,7 +1,39 @@
 
-jQuery(() => {
+let carrito= [];
 
-    $("#carrito").on("click", ()=>{
-        $("#titulo", "#precio").show(1000);  
-    });
-})   
+ /*agregar al carrito*/
+
+    $(".agregar").click (function() {
+        let articulo = {
+
+             nombre: $(this).attr("nombre"),
+             precio: $(this).attr("precio")
+        }
+        console.log(articulo);
+
+        carrito.push(articulo);
+   })
+
+ /*mostrar carrito*/
+
+   $(".carrito").click(function(){
+
+       console.log(carrito);
+
+   })
+
+
+   function total(){
+
+       let total =0;
+
+       for(i=0; i<carrito.length; i++){
+
+            total += Number(carrito[i].precio);
+       }
+       $(".total").text(total);
+    }
+
+
+
+
